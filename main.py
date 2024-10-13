df04c376bc1dcc220722d7a4d85c1cdf60460923
Ashutosh45-maker <ashutoshparadkar05@gmail.com>
2024-10-13 21:19:52 +0530

    functions

---
+++ main.py
@@ -0,0 +1,45 @@
+# def printname(name):
+#     print("my name is",name) 
+# n=input("enter your name:")
+# printname(n)
+
+# def printhobby(hobby):
+#     print("my favurite hobby is",hobby)
+# h=input("enter you favourite hobby:")
+# printhobby(h)
+
+#activity 2
+# def add(x,y):
+#     return x+y
+
+# def subtract(x,y):
+#     return x-y
+
+# def multiply(x,y):
+#     return x*y
+
+# def divide(x,y):
+#     return x/y
+
+# num1=int(input("Enter the first number==>"))
+# num2=int(input("Enter the second number==>"))
+
+# print("addition",add(num1,num2))
+# print("difference",subtract(num1,num2))
+# print("product",multiply(num1,num2))
+# print("division",divide(num1,num2))
+
+#activity 3
+
+# num=int(input("Enter the number==>"))
+# def positivenegative(x):
+#         if x>0:
+#              print("it is positive number",x)
+#         else:
+#              print("it is negative number",x)
+
+# positivenegative(num)
+
+#activity 4
+
+    
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+def printname(name):
+#     print("my name is",name) 
+# n=input("enter your name:")
+# printname(n)
